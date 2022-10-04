2c96bf3a1cfab35a4dc7fd55fb99bfe59947e8e9
arjunUpatel <arjunpatel151002@gmail.com>
2022-10-04 11:27:34 -0400

    executable modifications and code refactor

--- hw1/factor.c
+++ hw1/factor.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     long val = atol(argv[1]);
-    for(long j = 2; j <= val; j++) {
-        while(!(val % j)) {
+    for (long j = 2; j <= val; j++)
+    {
+        while (!(val % j))
+        {
             printf("%ld ", j);
             val /= j;
         }
