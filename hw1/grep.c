9a62ec0a6fbd199c21f9756f2ae82cf2405dcc22
AggravatedCow <114967693+AggravatedCow@users.noreply.github.com>
2022-10-05 20:24:21 -0400

    fixed max length bugs

---
+++ hw1/grep.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+    char* val;
+    int swi = 0;
+    if(argc == 1) {
+        printf("invalid input\n");
+        return 0;
+    }
+    else if(argv[1][0] == '-' &&  argv[1][1] == 'i'  && argv[1][2] == '\0' && argc == 3)  {
+        swi++;
+        val = argv[2];
+    }
+    else if(argc == 2) {
+        val = argv[1];
+    }
+    else {
+        printf("invalid input\n");
+        return 0;
+    }
+
+    int breakpt = 0;
+    while(1) {
+        long long size = 2;
+        int ind = 0;
+
+        char *o = (char*)malloc(size);
+        
+        while(1) {
+            if(scanf("%c",&o[ind]) == EOF) {
+                breakpt = 1;
+                break;
+            }
+            if(o[ind] == '\n') {
+                o[ind] = '\0';
+                break;
+            }
+
+            ind++;
+            
+            if(ind == size) {
+                char *temp = (char*)malloc(2*size);
+                for(int i = 0; i < size; i++) {
+                    temp[i] = o[i];
+                }
+                free(o);
+                o = temp;
+                size *= 2;
+            }
+        }
+
+        if(breakpt) {
+            free(o);
+            break;
+        }
+
+        int first = 0;
+        int indicator = 0;
+
+        while(1) {
+            int tf = 1;
+            int ind = 0;
+
+            while((o[ind+first] != '\0')) {
+                if(val[ind] == '\0') break;
+
+                if(swi) {
+                    int dist = abs(val[ind] - o[ind+first]);
+                    
+                    if(dist != 0 && dist != abs('A'-'a')) {
+                        tf = 0;
+                        break;
+                    }
+                }
+                else if(val[ind] != o[ind+first]) {
+                    tf = 0;
+                    break;
+                }
+
+                ind++;
+            }
+
+            if(!(val[ind] == '\0') && (o[ind+first] == '\0')) tf = 0;
+            if(tf) indicator = 1;
+            if(o[ind+first] == '\0' || indicator) break;
+
+            first++;
+        }
+        if(indicator) printf("%s\n", o);
+
+        free(o);
+    }
+
+    return 0;
+}
---
+++ hw1/uniq.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char **m = (char**)malloc(sizeof(char*));
+    long *ct = (long*)malloc(sizeof(long));
+
+    long len = 0;
+    long Max_Len = 1;
+    
+    int breakpt = 0;
+    while(1) {
+        long long size = 2;
+        int ind = 0;
+
+        char *o = (char*)malloc(size);
+        
+        while(1) {
+            if(scanf("%c",&o[ind]) == EOF) {
+                breakpt = 1;
+                break;
+            }
+            if(o[ind] == '\n') {
+                o[ind] = '\0';
+                break;
+            }
+
+            ind++;
+            
+            if(ind == size) {
+                char *temp = (char*)malloc(2*size);
+                for(int i = 0; i < size; i++) {
+                    temp[i] = o[i];
+                }
+                free(o);
+                o = temp;
+                size *= 2;
+            }
+        }
+
+        if(breakpt) {
+            free(o);
+            break;
+        }
+        
+        int indicator = 0;
+        if(len != 0) {
+            int i = len-1;
+            int tf = 1;
+            int ind = 0;
+            while((m[i][ind] != '\0') && (o[ind] != '\0')) {
+                if(m[i][ind] != o[ind]) {
+                    tf = 0;
+                    break;
+                }
+                ind++;
+            }
+            if((m[i][ind] == '\0') ^ (o[ind] == '\0')) tf = 0;
+
+            if(tf) {
+                ct[i]++;
+                indicator = 1;
+            }
+        
+        }
+
+        if(!indicator) {
+            m[len] = o;
+            ct[len] = 1;
+            len++;
+
+            if(len >= Max_Len) {
+                Max_Len *= 2;
+                char **tempArr = (char**)malloc((Max_Len)*sizeof(char*));
+                long *tempCt = (long*)malloc((Max_Len)*sizeof(long));
+
+                for(int i = 0; i < len; i++) {
+                    tempArr[i] = m[i];
+                    tempCt[i] = ct[i];
+                }
+
+                free(ct);
+                free(m);
+                ct = tempCt;
+                m = tempArr;
+            }
+        }
+        else free(o);
+    }
+
+    for(int i = 0; i < len; i++) {
+        printf("%ld %s\n",ct[i],m[i]);
+    }
+
+    for(int i = 0; i < len; i++) {
+        free(m[i]);
+    }
+    free(m);
+    free(ct);
+    
+    return 0;
+}
