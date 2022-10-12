ada6f86849a4a60ee24794ba866684583e43b1c2
ivanwang123 <ivanwang02@gmail.com>
2022-10-12 12:40:10 -0400

    Finished, need testing

--- hw2/tree.c
+++ hw2/tree.c
@@ -2,21 +2,72 @@
 #include<stdlib.h>
 #include<dirent.h>
 #include<string.h>
+#include<ctype.h>
 
-void printDir(char* dirname, int depth) {
+char* toLower(char* str) {
+  for (int i = 0; i < strlen(str); i++) {
+    str[i] = tolower(str[i]);
+  }
+  return str;
+}
+
+void printDir(char* path, int depth) {
   struct dirent* dir;
-  DIR* dirp = opendir(dirname);
+  DIR* dirp = opendir(path);
 
   if (dirp != NULL) {
+    int maxSize = 5;
+    int curSize = 0;
+    char** orderedDirs = calloc(maxSize, sizeof(char*));
+
     while ((dir = readdir(dirp)) != NULL) {
-      if (strcmp(dir->d_name, ".") != 0 && strcmp(dir->d_name, "..") != 0) {
-        for (int i = 0; i < depth; i++) {
-          printf("  ");
+      if (strcmp(dir->d_name, ".") == 0 || strcmp(dir->d_name, "..") == 0) {
+        continue;
+      }
+
+      curSize++;
+      if (curSize > maxSize) {
+        maxSize *= 2;
+        orderedDirs = realloc(orderedDirs, maxSize * sizeof(char*));
+      }
+
+      if (curSize == 1) {
+        orderedDirs[curSize-1] = dir->d_name;
+      } else {
+        for (int i = 0; i < curSize-1; i++) {
+          char orderedTemp[256];
+          char dirTemp[256];
+          strcpy(orderedTemp, orderedDirs[i]);
+          strcpy(dirTemp, dir->d_name);
+          if (strcmp(toLower(orderedTemp), toLower(dirTemp)) >= 0) {
+            for (int j = curSize-2; j >= i; j--) {
+              orderedDirs[j+1] = orderedDirs[j];
+            }
+            orderedDirs[i] = dir->d_name;
+            break;
+          }
+          if (i == curSize-2) {
+            orderedDirs[curSize-1] = dir->d_name;
+          }
         }
-        printf("- %s\n", dir->d_name);
-        printDir(dir->d_name, depth+1);
       }
     }
+
+    for (int i = 0; i < curSize; i++) {
+      for (int i = 0; i < depth; i++) {
+        printf("  ");
+      }
+      printf("- %s\n", orderedDirs[i]);
+
+      char* newPath = malloc(strlen(path) + strlen(orderedDirs[i]) + 2);
+      strcpy(newPath, path);
+      strcat(newPath, "/");
+      strcat(newPath, orderedDirs[i]);
+      printDir(newPath, depth+1);
+      free(newPath);
+    }
+
+    free(orderedDirs);
   }
 
   closedir(dirp);
