e0f6b92c3a53d6804a245eae04e8a2f164ce5a47
arjunUpatel <arjunpatel151002@gmail.com>
2022-12-02 14:36:19 -0500

    myinit and mycleanup basic functionality (tests needed)

--- hw4/mymalloc.c
+++ hw4/mymalloc.c
@@ -1,7 +1,21 @@
 #include <stdlib.h>
 
-void myinit(int allocAlg);
+static char *heap = NULL;
+static int alg = -1;
+
+void myinit(int allocAlg)
+{
+    heap = calloc(sizeof(char), 1024 * 1024);
+    alg = allocAlg;
+}
+
 void *mymalloc(size_t size);
 void myfree(void *ptr);
 void *myrealloc(void *ptr, size_t size);
-void mycleanup();
+
+void mycleanup()
+{
+    free(heap);
+    heap = NULL;
+    alg = -1;
+}
