b5b0c2c44d6f7821f1907b879f5eb4f00b98cf92
arjunUpatel <arjunpatel151002@gmail.com>
2022-12-02 22:01:05 -0500

    Indentation changes

--- hw4/mymalloc.c
+++ hw4/mymalloc.c
@@ -8,25 +8,25 @@ static char *searchPtr = NULL;
 // returns the pointer after the 4 blocks of int
 int insertInt(int num, int insertPos, char *heap)
 {
-    int mask = (1 << 8) - 1;
-    for (int i = 24, j = 0; i >= 0; i -= 8, j++)
-    {
-        char c = ((num >> i) & mask) + '0';
-        heap[insertPos] = c;
-        insertPos + j;
-    }
-    return insertPos + 1;
+	int mask = (1 << 8) - 1;
+	for (int i = 24, j = 0; i >= 0; i -= 8, j++)
+	{
+		char c = ((num >> i) & mask) + '0';
+		heap[insertPos] = c;
+		insertPos + j;
+	}
+	return insertPos + 1;
 }
 
 void myinit(int allocAlg)
 {
-    heap = calloc(sizeof(char), 1000000);
-    int insertPos = 0;
-    insertPos = insertInt(1000000, insertPos, heap);
-    insertPos = insertInt(-1, insertPos, heap);
-    insertPos = insertInt(-1, insertPos, heap);
-    root = insertPos;
-    alg = allocAlg;
+	heap = calloc(sizeof(char), 1000000);
+	int insertPos = 0;
+	insertPos = insertInt(1000000, insertPos, heap);
+	insertPos = insertInt(-1, insertPos, heap);
+	insertPos = insertInt(-1, insertPos, heap);
+	root = insertPos;
+	alg = allocAlg;
 }
 
 void *mymalloc(size_t size);
@@ -35,9 +35,9 @@ void *myrealloc(void *ptr, size_t size);
 
 void mycleanup()
 {
-    free(heap);
-    heap = NULL;
-    root = -1;
-    searchPtr = NULL;
-    alg = -1;
+	free(heap);
+	heap = NULL;
+	root = -1;
+	searchPtr = NULL;
+	alg = -1;
 }
