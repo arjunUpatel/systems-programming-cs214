2c3a5c992f48a0f19b8957d2f6caad6f5546c019
ivanwang123 <ivanwang02@gmail.com>
2022-12-04 11:32:41 -0500

    Change char to unsigned char

--- hw4/mymalloc.c
+++ hw4/mymalloc.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 #include <limits.h>
 
-static char *heap = NULL;
+static unsigned char *heap = NULL;
 static int alg = -1;
 static int root = -1;
 static char *searchPtr = NULL;
 
 // returns the pointer after the 4 blocks of int
-int insertInt(int num, int insertPos, char *heap)
+int insertInt(int num, int insertPos, unsigned char *heap)
 {
   for (int i = 24; i >= 0; i -= 8)
   {
-    char c = num >> i;
+    unsigned char c = num >> i;
     heap[insertPos] = c;
     insertPos++;
   }
@@ -31,13 +31,13 @@ void printHeap(int heapSize)
 void myinit(int allocAlg)
 {
   int heapSize = 32;
-  heap = calloc(sizeof(char), heapSize);
+  heap = calloc(sizeof(unsigned char), heapSize);
   int insertPos = 0;
   // insert size in front
   insertPos = insertInt(heapSize, 0, heap);
   printHeap(heapSize);
   // insert next ptr
-  insertPos = insertInt(1, insertPos, heap);
+  insertPos = insertInt(100000, insertPos, heap);
   printHeap(heapSize);
   // inster prev ptr
   insertPos = insertInt(1, insertPos, heap);
