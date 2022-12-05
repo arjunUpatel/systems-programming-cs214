9e7278bda97390e5301bbfc438414ba01c18a6d0
arjunUpatel <arjunpatel151002@gmail.com>
2022-12-04 20:22:49 -0500

    Progress on mymalloc

--- hw4/mymalloc.c
+++ hw4/mymalloc.c
@@ -6,7 +6,7 @@ const BLOCK_SIZE = 4;
 const NEXT_PTR = 4;
 const PREV_PTR = 4;
 const FOOTER_SIZE = 4;
-const MEMORY_SIZE = 1000000;
+const MEMORY_SIZE = 1024 * 1024;
 const ALIGNMENT = 8;
 const NULL_PTR = -1;
 
@@ -47,12 +47,32 @@ int getBlockSize(int p, char *heap)
 
 int getNextPtr(int p, char *heap)
 {
-  return getInt(p - HEADER_SIZE - BLOCK_SIZE, heap);
+  return getInt(p - HEADER_SIZE + BLOCK_SIZE, heap);
 }
 
 int getPrevPtr(int p, char *heap)
 {
-  return getInt(p - HEADER_SIZE - BLOCK_SIZE - NEXT_PTR, heap);
+  return getInt(p - HEADER_SIZE + BLOCK_SIZE + NEXT_PTR, heap);
+}
+
+void setHeaderSize(int p, int newSize, char *heap)
+{
+  insertInt(newSize, p - HEADER_SIZE, heap);
+}
+
+void setNextPtr(int p, int newNext, char *heap)
+{
+  insertInt(newNext, p - HEADER_SIZE + BLOCK_SIZE, heap);
+}
+
+void setPrevPtr(int p, int newPrev, char *heap)
+{
+  insertInt(newPrev, p - HEADER_SIZE + BLOCK_SIZE + NEXT_PTR, heap);
+}
+
+void setFooterSize(int p, int blockSize, char *heap)
+{
+  insertInt(blockSize, p + blockSize - HEADER_SIZE - FOOTER_SIZE, heap);
 }
 
 size_t spaceCalc(size_t size)
@@ -66,34 +86,64 @@ size_t spaceCalc(size_t size)
 int splitBlock(int p, int spaceNeeded, int chosenBlockSize, char *heap)
 {
   int splitSize = chosenBlockSize - spaceNeeded;
+  int split_p = p + spaceNeeded + HEADER_SIZE;
   // change size of chosen block in header
-  insertInt(spaceNeeded, p - HEADER_SIZE, heap);
+  setHeaderSize(p, spaceNeeded, heap);
   // update size of chosen block in footer
-  int insertPos = insertInt(spaceNeeded, p + spaceNeeded - FOOTER_SIZE, heap);
-  // change size of split block in footer
-  insertInt(splitSize, insertPos + splitSize - FOOTER_SIZE, heap);
+  setFooterSize(p, spaceNeeded, heap);
   // change size of split block in header
-  insertPos = insertInt(splitSize, insertPos, heap);
+  setHeaderSize(split_p, splitSize, heap);
+  // change size of split block in footer
+  setFooterSize(split_p, splitSize, heap);
   // copy next ptr into split
-  insertPos = insertInt(getInt(p + BLOCK_SIZE, heap), insertPos, heap);
+  setNextPtr(split_p, getNextPtr(p, heap), heap);
   // copy prev ptr into split
-  return insertInt(getInt(p + BLOCK_SIZE + NEXT_PTR, heap), insertPos, heap);
+  setPrevPtr(split_p, getPrevPtr(p, heap), heap);
+  return split_p;
 }
 
 void myinit(int allocAlg)
 {
+  int p = HEADER_SIZE;
   heap = calloc(sizeof(char), MEMORY_SIZE);
-  int insertPos = 0;
   // insert size in front
-  insertPos = insertInt(MEMORY_SIZE, 0, heap);
+  setHeaderSize(p, MEMORY_SIZE, heap);
   // insert next ptr
-  insertPos = insertInt(NULL_PTR, insertPos, heap);
+  setNextPtr(p, NULL_PTR, heap);
   // insert prev ptr
-  insertPos = insertInt(NULL_PTR, insertPos, heap);
+  setPrevPtr(p, NULL_PTR, heap);
   // insert size in end
-  insertInt(MEMORY_SIZE, MEMORY_SIZE - FOOTER_SIZE, heap);
-  root = insertPos;
+  setFooterSize(p, MEMORY_SIZE, heap);
+  root = p;
   alg = allocAlg;
+}
+
+void updatePtrs(int p, int blockWasSplit, char *heap)
+{
+  if (blockWasSplit)
+  {
+    if (getNextPtr(p, heap) == -1)
+      setNextPtr(getPrevPtr(p, heap), p, heap);
+    if (getPrevPtr(p, heap) == -1)
+      root = getNextPtr(p, heap);
+    else
+    {
+      setNextPtr(getPrevPtr(p, heap), getNextPtr(p, heap), heap);
+      setPrevPtr(getNextPtr(p, heap), getPrevPtr(p, heap), heap);
+    }
+  }
+  else
+  {
+    if (getNextPtr(p, heap) == -1)
+      setNextPtr(getPrevPtr(p, heap), NULL_PTR, heap);
+    if (getPrevPtr(p, heap) == -1)
+      root = getNextPtr(p, heap);
+    else
+    {
+      setNextPtr(getPrevPtr(p, heap), p, heap);
+      setPrevPtr(getNextPtr(p, heap), p, heap);
+    }
+  }
 }
 
 void *mymalloc(size_t size)
@@ -112,24 +162,8 @@ void *mymalloc(size_t size)
     int update_p = p;
     if (spaceNeeded < chosenBlockSize)
       update_p = splitBlock(p, spaceNeeded, chosenBlockSize, heap);
-    // TODO: update links of prev and next block
-    // if (update_p == p)
-    // {
-    //   // make next and prev skip curr block
-    //   // cases
-    //   // next null
-    //   // prev null
-    //   //
-    // }
-    // else
-    // {
-    //   // make next and prev point to curr block
-    //   // update next of prev
-    //   if (getNextPtr(update_p, heap) != -1)
-    //     insertInt(update_p, getNextPtr(update_p, heap), heap);
-    //   if (getPrevPtr(update_p, heap) != -1)
-    //     insertInt(update_p, getPrevPtr(update_p, heap), heap);
-    // }
+    int blockWasSplit = update_p == p ? 1 : 0;
+    updatePtrs(update_p, blockWasSplit, heap);
     return heap + p;
   }
 }
